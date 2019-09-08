a52e3cbbd36db0cc3d88c18829557d37904f565c
Guruprasad Sridharan <guruprasad.sridharan@phonepe.com>
2019-09-08 13:25:15 +0530

    Add instrumentation for connection setup times

--- metrics-okhttp/src/main/java/com/raskasa/metrics/okhttp/ConnectionInterceptor.java
+++ metrics-okhttp/src/main/java/com/raskasa/metrics/okhttp/ConnectionInterceptor.java
@@ -1,13 +1,16 @@
 package com.raskasa.metrics.okhttp;
 
+import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * An {@link Interceptor} that monitors the number of submitted, running, and
@@ -19,17 +22,31 @@ final class ConnectionInterceptor extends EventListener {
     private final Meter failed;
     private final Meter acquired;
     private final Meter released;
+    private final Histogram setupTimes;
+    private final ConcurrentHashMap<InetSocketAddress, Long> initTimes;
 
     ConnectionInterceptor(MetricRegistry registry, String name) {
         this.requests = registry.meter(MetricRegistry.name(name, "connection-requests"));
         this.failed = registry.meter(MetricRegistry.name(name, "connection-failed"));
         this.acquired = registry.meter(MetricRegistry.name(name, "connection-acquired"));
         this.released = registry.meter(MetricRegistry.name(name, "connection-released"));
+        this.setupTimes = registry.histogram(MetricRegistry.name(name, "connection-setup"));
+        // TODO: configure map on the basis of connection pool size
+        this.initTimes = new ConcurrentHashMap<>(128, 0.75f, 16);
     }
 
     @Override
     public void connectStart(Call call, InetSocketAddress inetSocketAddress, Proxy proxy) {
         requests.mark();
+        initTimes.put(inetSocketAddress, System.currentTimeMillis());
+    }
+
+    @Override
+    public void connectEnd(Call call, InetSocketAddress inetSocketAddress, Proxy proxy, Protocol protocol) {
+        Long initTime = initTimes.get(inetSocketAddress);
+        if (initTime != null) {
+            setupTimes.update(System.currentTimeMillis() - initTime);
+        }
     }
 
     @Override
